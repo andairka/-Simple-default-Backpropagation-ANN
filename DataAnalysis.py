c21e2ee0a5d9aabf9a7a6279c8e27a826ceeb096
Andairka <andairka@wp.pl>
2020-06-30 23:21:03 +0200

    Finish Data Analysis

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,7 +1,8 @@
 import ImportTitanicData
 import DataPreparation
 
-class DataAnaliysis():
+# analiza danych przed preparacją danych
+class DataAnaliysisBefore():
     def showTrain(self):
         importData = ImportTitanicData.DataImport()
         train = importData.importTrain()
@@ -24,6 +25,29 @@ class DataAnaliysis():
     def dtypesTest(self):
         return self.showTest().dtypes
 
+# analiza danych po preparacji danych
+class DataAnaliysisAfter():
+    def showTrain(self):
+        dataPreparation = DataPreparation.DataPreparation()
+        train = dataPreparation.prepareTrainData()
+        return train
+
+    def shapeTrain(self):
+        return self.showTrain().shape
+
+    def dtypesTrain(self):
+        return self.showTrain().dtypes
+
+    def showTest(self):
+        dataPreparation = DataPreparation.DataPreparation()
+        test = dataPreparation.prepareTestData()
+        return test
+
+    def shapeTest(self):
+        return self.showTest().shape
+
+    def dtypesTest(self):
+        return self.showTest().dtypes
 
     #
     #
@@ -35,8 +59,8 @@ class DataAnaliysis():
 
 
 
-
-dataAnaysis = DataAnaliysis()
+# dataAnaysis = DataAnaliysisBefore()
+dataAnaysis = DataAnaliysisAfter()
 
 # print('czesio')
 # print('Analiza danych przed wypełnieniem NaN')
@@ -44,6 +68,11 @@ dataAnaysis = DataAnaliysis()
 # print('\n\nshape Train\n', dataAnaysis.shapeTrain())
 # print('\n\ndtypes Train\n', dataAnaysis.dtypesTrain())
 
-dataPreparation = DataPreparation.DataPreparation()
-print(dataPreparation.prepareTrainData().to_string())
+print('Analiza danych po wypełnieniem NaN i preparacji danych')
+print('Tabela Train\n', dataAnaysis.showTrain())
+print('\n\nshape Train\n', dataAnaysis.shapeTrain())
+print('\n\ndtypes Train\n', dataAnaysis.dtypesTrain())
+
+# dataPreparation = DataPreparation.DataPreparation()
+# print(dataPreparation.prepareTrainData().to_string())
 
