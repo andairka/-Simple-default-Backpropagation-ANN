c5e50def709121c53a8fcf640200142e5bc17be9
Andairka <andairka@wp.pl>
2020-07-02 00:49:03 +0200

    to co mam

--- DataPreparation.py
+++ DataPreparation.py
@@ -31,17 +31,17 @@ class DataPreparation:
 
         dataSet = dataImport.importTrain()
 
-        # kolumny 'Embarked', 'Ticket', 'Sex', 'Name', 'Cabin' według mnie dane nie są istotne
+        # kolumny 'Embarked', 'Ticket', 'Name', 'Cabin' według mnie dane nie są istotne
         # dla uczenia sieci neuronowej dlatego pomijam ich uzupełnianie oraz usuwam je z danych wejściowych
 
         dataSet.drop(['Embarked'], axis=1, inplace=True)
         dataSet.drop(['Ticket'], axis=1, inplace=True)
-        dataSet.drop(['Sex'], axis=1, inplace=True)
+        # dataSet.drop(['Sex'], axis=1, inplace=True)
         dataSet.drop(['Name'], axis=1, inplace=True)
         dataSet.drop(['Cabin'], axis=1, inplace=True)
 
         # kolumnę 'Survived' pomijamy, bo to jest wynik uczenia się naszej sieci
-        dataSet.drop(['Survived'], axis=1, inplace=True)
+        # dataSet.drop(['Survived'], axis=1, inplace=True)
 
         # w kolumnach 'PasengerId', 'Pclass', 'SibSp', 'Parch' nie brakuje danych i ich nie zmieniam
 
@@ -51,9 +51,16 @@ class DataPreparation:
         # zmiana NaN w kolumnie 'Age', poprzez medianę
         dataSet['Age'].fillna(dataSet['Age'].mean(), inplace=True)
 
-        # print(dataSet.to_string())
+        # zmiana typu danych w kolumnie 'Sex'
+        genders = {"male": 0, "female": 1}
+        dataSet['Sex'] = dataSet['Sex'].map(genders)
+
+        print(dataSet.to_string())
         return dataSet
 
 
+dataPreparaion = DataPreparation()
+
+print(dataPreparaion.prepareTrainData())
 
 
